c26cb788f0116106d50624b58d2f819c0e031baf
SunitaHosur <sunitahosur81@gmail.com>
2020-12-15 22:51:25 +0530

    Find Number is Positive or negative

---
+++ PositiveNegative.java
@@ -0,0 +1,17 @@
+public class PosNeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num=8;
+    		  if(num<0){
+    			  System.out.println("Number is Negative");
+    		  }
+    		  else if(num>0){
+    			  System.out.println("Number is Positive");
+    		  }
+    		  else{
+    			  System.out.println("Number is Zero");
+    		  }
+	}
+
+}
