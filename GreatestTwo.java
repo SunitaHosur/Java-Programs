0660be5aa5a9836c62147da8d8339ad235a229f3
SunitaHosur <sunitahosur81@gmail.com>
2020-12-15 22:49:27 +0530

    Find Greatest of two numbers

---
+++ GreatestTwo.java
@@ -0,0 +1,21 @@
+public class GretestTwo {
+
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+		num1=7;
+		num2=5;
+		if(num1>num2)
+		{
+			System.out.println("num1 is Greater");
+		}
+		else if(num2>num1)
+		{
+			System.out.println("num1 is Smaller");
+		}else{
+			System.out.println("numbers are equal.");
+		}
+
+	}
+
+}
