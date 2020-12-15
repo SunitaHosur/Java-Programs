e2ce10d238e86c0c0e1c1ca7071ca5a1209385e1
SunitaHosur <sunitahosur81@gmail.com>
2020-12-15 22:47:06 +0530

    Check Number is even or odd

---
+++ EvenOddCheck.java
@@ -0,0 +1,17 @@
+public class EvenOddCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=11;
+		if(num%2==0)
+		{
+			System.out.println("Number is even");
+		}
+		else{
+			System.out.println("Number is odd");
+		}
+
+	}
+
+}
